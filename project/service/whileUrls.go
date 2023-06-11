5a85ed3d6b6e4a67b7c584fdb5c9c3a7102b204c
Lxb921006 <120332269@qq.com>
2023-06-11 22:29:07 +0800

    websocket优化

--- project/service/whileUrls.go
+++ project/service/whileUrls.go
@@ -12,14 +12,8 @@ func (WhileUrls) WhileList(url string) bool {
 		"/login",
 		"/logout",
 		"/galogin",
-		// test assets
 		"/assets/ws",
-		"/assets/api",
-		"/assets/process/status",
-		"/assets/process/update/create",
-		"/assets/process/update/list",
-		"/assets/list",
-		"/assets/upload",
+		//"/assets/api",
 	}
 
 	for i := 0; i < len(wls); i++ {
@@ -45,14 +39,8 @@ func (WhileUrls) OperateWhileList(url string) bool {
 		"/role/rolesname",
 		"/role/userperms",
 		"/user/getinfobyname",
-		// test assets
-		"/assets/ws",
-		"/assets/api",
-		"/assets/process/status",
-		"/assets/process/update/create",
-		"/assets/process/update/list",
 		"/assets/list",
-		"/assets/upload",
+		"/assets/process/update/list",
 	}
 
 	for i := 0; i < len(wls); i++ {
