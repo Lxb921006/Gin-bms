1fd3202208a1d4d10b309ccb6d40ebe05a6211e5
Lxb921006 <120332269@qq.com>
2024-03-27 16:22:06 +0800

    添加连接redis，初始表的判断

--- project/main.go
+++ project/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"github.com/Lxb921006/Gin-bms/project/migrate"
 	"log"
 
 	"github.com/Lxb921006/Gin-bms/project/dao"
-	"github.com/Lxb921006/Gin-bms/project/migrate"
 	"github.com/Lxb921006/Gin-bms/project/router/root"
 )
 
 func main() {
 	//初始化mysql
 	err := dao.InitPoolMysql()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	//这里初始化数据库表
+	err = migrate.InitTable()
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
@@ -24,13 +30,8 @@ func main() {
 	if dao.RdPool == nil {
 		log.Fatalf(dao.ErrorRedisConnectFailed.Error())
 	}
-	dao.Rds = dao.NewRedisDb(dao.RdPool, map[string]dao.Md{})
 
-	//这里初始化数据库表
-	err = migrate.InitTable()
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
+	dao.Rds = dao.NewRedisDb(dao.RdPool, map[string]dao.Md{})
 
 	//初始化gin并启动
 	t := root.SetupRouter()
